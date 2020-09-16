269d793a9bd5cc4058d7f0230943b806d9c820ea
MaksimPogulyaev <70638320+MaksimPogulyaev@users.noreply.github.com>
2020-09-16 10:57:30 +0300

    Add files via upload

---
+++ Prac3/MovRectangle.java
@@ -0,0 +1,21 @@
+package Prac3;
+
+class MoveRectangle implements Movable{
+    private MovablePoint topleft;
+    private MovablePoint bottomRight;
+
+    public void MovableRectangle(int x1, int y1, int x2, int y2)
+    {
+    }
+
+    public void moveUp() {}
+
+    @Override
+    public void moveUP() {
+
+    }
+
+    public void moveDown() {}
+    public void moveLeft() {}
+    public void moveRight() {}
+}
