fb8dff17cff4a6c9876601847c63b0973299658a
MaksimPogulyaev <70638320+MaksimPogulyaev@users.noreply.github.com>
2020-09-28 16:48:08 +0300

    Add files via upload

---
+++ Practica5/Zadanie6.java
@@ -0,0 +1,29 @@
+public class Zadanie6 {
+    private static int recursion(int n, int var)
+    {
+        if (n > 1)
+        {
+            if (n % var != 0)
+            {
+                return recursion(n, var+1);
+            } else if (n % var == 0)
+            {
+                if (var == n)
+                {
+                    System.out.println("YES");
+                }
+                else
+                {
+                    System.out.println("NO");
+                }
+                return 0;
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args)
+    {
+        recursion(5,2);
+        recursion(4,2);
+    }
+}
