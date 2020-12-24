9efeddf116b6a85ed4e1bebcd47ec0eeceadbe2a
MaksimPogulyaev <70638320+MaksimPogulyaev@users.noreply.github.com>
2020-12-24 13:08:44 +0300

    Add files via upload

---
+++ Practica8/UnfairWaitList.java
@@ -0,0 +1,16 @@
+class UnfairWaitList<E> extends WaitList<E> {
+
+    public UnfairWaitList() {
+    }
+
+    void remove(E element) {
+        content.remove(element);
+    }
+
+    void moveToBack(E element) {
+        if (content.remove(element))
+        {
+            content.add(element);
+        }
+    }
+} 
